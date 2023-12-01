9e650540b2015340f35dda962ff0de4abba93532
StephanGuruShots <stephan.k@gurushots.com>
2023-12-01 15:27:12 +0200

    another commit

--- app/src/main/java/com/example/socketiotest/MainActivity.kt
+++ app/src/main/java/com/example/socketiotest/MainActivity.kt
@@ -1,33 +1,24 @@
 package com.example.socketiotest
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.socketiotest.ui.theme.SocketIoTestTheme
 import io.socket.client.IO
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import org.json.JSONObject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val mSocket = IO.socket("http://localhost:3000")
+        val mSocket = IO.socket("http://10.0.2.2:3000")
         mSocket.connect()
-        if (!mSocket.isActive) Log.d("rawr", "doesn't work")
-        if (mSocket.isActive) Log.d("rawr", "works")
 
         setContent {
             SocketIoTestTheme {
@@ -36,48 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    var textFromOnLogin: String = "my"
-
-                    LaunchedEffect(key1 = null, block = {
-                        mSocket.on("add user") {
-                            textFromOnLogin = (it[0] as JSONObject).getInt("numUsers").toString()
-                        }
-
-                        mSocket.on("new message") {
-                            textFromOnLogin =
-                                (it[0] as JSONObject).getString("new message").toString()
-                        }
-
-                        mSocket.onAnyOutgoing() {
-                            Log.d("rawr", "going")
-                        }
-                        mSocket.connect()
-                    })
-
-                    Column {
-
-                        Button(onClick = { mSocket.emit("chat message", "android here") }) {
-                            Text("sample button")
-                        }
-
-                        Button(onClick = {
-                            // perform the user login attempt.
-
-                            // perform the sending message attempt.
-                            mSocket.emit("add user", "Man1")
-                        }) {
-                            Text(text = "\"addUser\", \"Man1\"")
-                        }
-                        Text(text = textFromOnLogin)
-                        Button(onClick = {
-                            // perform the user login attempt.
-
-                            // perform the sending message attempt.
-                            mSocket.emit("new message", "message")
-                        }) {
-                            Text(text = "\"new message\", \"message\"")
-                        }
-                    }
+                    SampleFirstScreen(socket = mSocket)
                 }
             }
         }
---
+++ app/src/main/java/com/example/socketiotest/SampleFirstScreen.kt
@@ -0,0 +1,111 @@
+package com.example.socketiotest
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.socket.client.Ack
+import io.socket.client.Socket
+
+data class Message(
+    val from: String,
+    val value: String
+)
+
+@Composable
+fun SampleFirstScreen(
+    socket: Socket
+) {
+
+    val listMessages = remember {
+        mutableStateOf(listOf<Message>())
+    }
+
+    LaunchedEffect(key1 = null) {
+        socket.on("hi") {
+            listMessages.add(Message("server", "${it[0]}"))
+        }
+
+        socket.on("args") {
+            listMessages.add(Message("server", "${it.toList()}"))
+        }
+
+        socket.on("ack from server") {
+            listMessages.add(Message("server", "it[0] is Ack = ${it[0] is Ack}"))
+            (it[0] as Ack).call("hi server")
+            listMessages.add(Message("client", "\"hi server\""))
+        }
+    }
+
+    Row {
+        Column {
+            Button(onClick = {
+                socket.emit("hi", "hi from client")
+                listMessages.add(Message("client", "hi from client"))
+            }) {
+                Text(text = "Say hi")
+            }
+
+            Button(onClick = {
+                socket.emit("args", "1", 2, 3.0, Message("me", "ssage"))
+                listMessages.add(Message("client", "\"1\", 2, 3.0, Message(\"me\", \"ssage\")"))
+            }) {
+                Text(text = "Say args")
+            }
+
+            Button(onClick = {
+                socket.emit("ack from client", "2", 3, object : Ack {
+                    override fun call(vararg args: Any?) {
+                        val response = args[0]
+                        listMessages.add(Message("client response from server", "$response"))
+                    }
+                })
+                listMessages.add(Message("client", "\"2\", 3, object : Ack"))
+            }) {
+                Text(text = "ack from client")
+            }
+
+            Button(onClick = {
+                socket.emit("ack from server init", "ack me")
+                listMessages.add(Message("client", "ack me"))
+            }) {
+                Text(text = "ack from server")
+            }
+        }
+
+        Column {
+            LazyColumn() {
+                items(listMessages.value) {
+                    Card {
+                        Row {
+                            Text(modifier = Modifier.weight(1f), text = it.from)
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(modifier = Modifier.weight(1f), text = it.value)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                }
+            }
+        }
+    }
+}
+
+fun MutableState<List<Message>>.add(message: Message) {
+    val newList: MutableList<Message> = mutableListOf<Message>()
+    newList.addAll(this.value)
+    newList.add(message)
+    this.value = newList
+}
